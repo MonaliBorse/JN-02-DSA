40f4e1cd3ffd817f23a8908a8cc49e0448c2f821
MonaliBorse <75569192+MonaliBorse@users.noreply.github.com>
2023-07-12 21:38:10 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+                                    SelectionSort
+*******************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int n;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	cout<<"\nEnter the element into array: ";
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[j]<arr[i])
+			{
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+		}
+	}
+	cout<<"Sorted aaray is: ";
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
